78fbd1b41ade232b2a27e7e7054c69251cc86a50
mateo1423 <75327637+mateo1423@users.noreply.github.com>
2021-05-03 20:15:26 +0200

    Add files via upload

---
+++ store.js
@@ -0,0 +1,32 @@
+let data = {
+    podaci: {
+     data: [
+           { ime_aplikacije: 'health_and_fitness' },
+        ],
+    },
+    Vježbe:{
+        data: {
+            Ruke:{
+                test: 'test',
+            },
+            Noge:{
+                test: 'test',
+            },
+            Leda:{
+                test: 'test',
+            },
+            prsa:{
+                test: 'test',
+            },
+            Core:{
+                test: 'test',
+            },
+            Ramena:{
+                test: 'test',
+            },
+        },
+    },
+
+};
+
+export default data;
