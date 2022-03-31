132b8031740624a44196d28c93d72081583be6ca
ManuelFabijancic1 <75333397+ManuelFabijancic1@users.noreply.github.com>
2022-03-31 17:51:28 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import data from './store';
 import cors from 'cors';
 
 const app = express(); // instanciranje aplikacije
-const port = 3200; // port na kojem će web server slušati
+const port = 3000; // port na kojem će web server slušati
 
 app.use(cors());
 app.use(express.json());
@@ -15,11 +15,9 @@ app.get('/podaci', (req, res) => res.json(data.podaci));
 app.get('/vjezbe', (req, res) => res.json(data.vjezbe));
 
 //pojedine vjezbe
-app.get('/vjezbe/:ruke', (req, res) => res.json(data.vjezbe));
-app.get('/vjezbe/:noge', (req, res) => res.json(data.vjezbe));
-app.get('/vjezbe/:prsa', (req, res) => res.json(data.vjezbe));
-app.get('/vjezbe/:leda', (req, res) => res.json(data.vjezbe));
-app.get('/vjezbe/:core', (req, res) => res.json(data.vjezbe));
-app.get('/vjezbe/:ramena', (req, res) => res.json(data.vjezbe));
+app.get('/vjezbe/:arms', (req, res) => res.json(data.vjezbe));
+app.get('/vjezbe/:legs', (req, res) => res.json(data.vjezbe));
+app.get('/vjezbe/:abs', (req, res) => res.json(data.vjezbe));
+app.get('/vjezbe/:back', (req, res) => res.json(data.vjezbe));
 
 app.listen(port, () => console.log(`Port: ${port}`));
