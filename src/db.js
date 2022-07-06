4c5adf1f0486200c7de650552ba3f3e0c41b0588
ManuelFabijancic1 <75333397+ManuelFabijancic1@users.noreply.github.com>
2022-07-06 21:08:38 +0200

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,33 +1,32 @@
-let MongoClient = require('mongodb').MongoClient;
 
+import mongo from 'mongodb';
+
+// let connection_string = 'mongodb+srv://admin:admin@nt-cluster-jmi8g.mongodb.net/fipugram?retryWrites=true&w=majority';
 let connection_string = process.env.CONNECTION_STRING;
 
-let client = new MongoClient(connection_string, {
- useNewUrlParser: true,
- useUnifiedTopology: true
+let client = new mongo.MongoClient(connection_string, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
-let db = null
+let db = null;
 
-
+// eksportamo Promise koji resolva na konekciju
 export default () => {
- return new Promise((resolve, reject) => {
-
-    
- if (db && client.isConnected) {
- resolve(db)
- }
- else {
- client.connect(err => {
- if (err) {
- reject("Spajanje na bazu nije uspjelo:" + err);
- }
- else {
- console.log("Database connected successfully!");
- db = client.db("vjezbe");
- resolve(db);
- }
- });
- }
- });
-}
+    return new Promise((resolve, reject) => {
+        // ako smo inicijalizirali bazu i klijent je još uvijek spojen
+        if (db && client.isConnected()) {
+            resolve(db);
+        } else {
+            client.connect((err) => {
+                if (err) {
+                    reject('Spajanje na bazu nije uspjelo:' + err);
+                } else {
+                    console.log('Database connected successfully!');
+                    db = client.db('fipugram');
+                    resolve(db);
+                }
+            });
+        }
+    });
+};
