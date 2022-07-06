6cb3c40ce6402b7db40a4cf5850434a11b6e9d9c
ManuelFabijancic1 <75333397+ManuelFabijancic1@users.noreply.github.com>
2022-07-06 22:33:27 +0200

    Update db.js

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ import mongo from 'mongodb';
 // let connection_string = 'mongodb+srv://admin:admin@nt-cluster-jmi8g.mongodb.net/fipugram?retryWrites=true&w=majority';
 let connection_string = process.env.MONGODB_URI;
 
-let client = new mongo.MongoClient(connection_string, {
+let client = await new mongo.MongoClient(connection_string, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
